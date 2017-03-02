03eda7898b5681aff81f5ff3869b6f41d53aeda7
Dylan Simon <dylan@dylex.net>
2017-03-02 08:52:38 -0500

    Add nyupress DLTS solr core

--- Source/DLTS.hs
+++ Source/DLTS.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module Source.DLTS
   ( DLTSCore(..)
   , loadDLTS
@@ -18,34 +19,42 @@ import           Source.Solr
 data DLTSCore
   = DLTSCore
   | DLTSViewer
+  | DLTSPress
   deriving (Eq, Enum, Bounded, Show)
 
 instance JSON.FromJSON DLTSCore where
   parseJSON = JSON.withText "DLTS core" cn where
     cn "core" = return DLTSCore
     cn "viewer" = return DLTSViewer
+    cn "nyupress" = return DLTSPress
     cn _ = fail "Unknown DLTS core"
 
 dltsBase :: HTTP.Request
 dltsBase = HTTP.parseRequest_ "http://discovery.dlib.nyu.edu:8080/solr3_discovery"
 
-dltsRequest :: DLTSCore -> HTTP.Request
-dltsRequest DLTSCore = addRequestPath dltsBase "select"
-dltsRequest DLTSViewer = addRequestPath dltsBase "viewer/select"
 
-dltsCollectionFields :: DLTSCore -> (T.Text, T.Text)
-dltsCollectionFields DLTSCore = ("collection_code", "collection_title")
-dltsCollectionFields DLTSViewer = ("sm_collection_code", "sm_collection_label")
+data DLTSCoreMeta = DLTSCoreMeta
+  { dltsRequest :: !HTTP.Request
+  , dltsCollectionCode, dltsCollectionName, dltsHandle, dltsChanged :: !T.Text
+  }
+
+dltsCoreMeta :: DLTSCore -> DLTSCoreMeta
+dltsCoreMeta DLTSCore   = DLTSCoreMeta (addRequestPath dltsBase "select")
+  "collection_code" "collection_title" "ss_handle" "ds_changed"
+dltsCoreMeta DLTSViewer = DLTSCoreMeta (addRequestPath dltsBase "viewer/select")
+  "sm_collection_code" "sm_collection_label" "ss_handle" "ds_changed"
+dltsCoreMeta DLTSPress  = DLTSCoreMeta (addRequestPath dltsBase "nyupress/select")
+  "collection_code" "publisher" "handle" "timestamp"
 
 loadDLTS :: DLTSCore -> T.Text -> HSet.HashSet T.Text -> IO Documents
-loadDLTS core c fl = parseM (mapM doc) =<< loadSolr (dltsRequest core) (TE.encodeUtf8 $ code <> T.cons ':' c) (fl <> fl') where
-  (code, label) = dltsCollectionFields core
-  fl' = HSet.fromList ["ss_handle", code, label, "ds_changed"]
+loadDLTS core c fl = parseM (mapM doc) =<< loadSolr dltsRequest (TE.encodeUtf8 $ dltsCollectionCode <> T.cons ':' c) (fl <> fl') where
+  DLTSCoreMeta{..} = dltsCoreMeta core
+  fl' = HSet.fromList [dltsCollectionCode, dltsCollectionName, dltsHandle, dltsChanged]
   doc o = do
-    hdl <- maybe (fail "invalid handle") return . T.stripPrefix "http://hdl.handle.net/2333.1/" =<< o JSON..: "ss_handle"
-    cc <- one =<< o JSON..: code
-    cl <- one =<< o JSON..: label
-    m <- o JSON..: "ds_changed"
+    hdl <- maybe (fail "invalid handle") return . T.stripPrefix "http://hdl.handle.net/2333.1/" =<< o JSON..: dltsHandle
+    cc <- one =<< o JSON..: dltsCollectionCode
+    cl <- one =<< o JSON..: dltsCollectionName
+    m <- o JSON..: dltsChanged
     o' <- mapM JSON.parseJSON o
     return Document
       { documentID = cc <> ":hdl-handle-net-2333-1-" <> hdl
