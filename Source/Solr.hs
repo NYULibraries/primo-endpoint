74430f08022d106c87d7f19b46aaa6ddead8d5aa
Dylan Simon <dylan@dylex.net>
2017-03-03 10:27:34 -0500

    Revert "Generalize pagination handling"
    This reverts commit 49e3b488bcad3aa6b43f7f619ca18e0fdf072fec.

--- Source/Solr.hs
+++ Source/Solr.hs
@@ -8,6 +8,7 @@ module Source.Solr
 import qualified Data.Aeson.Types as JSON
 import qualified Data.ByteString.Char8 as BSC
 import qualified Data.HashSet as HSet
+import           Data.Monoid ((<>))
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE (encodeUtf8)
 import qualified Data.Text.Read as TR (decimal)
@@ -15,23 +16,30 @@ import qualified Data.Vector as V
 import qualified Network.HTTP.Client as HTTP
 import qualified Network.HTTP.Simple as HTTP
 
-import           Util
-import           Paginate
+data SolrResponse = SolrResponse
+  { solrStart, solrNumFound :: Int
+  , solrDocs :: V.Vector JSON.Object
+  } deriving (Show)
 
 parseInt :: JSON.Value -> JSON.Parser Int
 parseInt (JSON.String (TR.decimal -> Right (n, ""))) = return n -- drupal can have a string here
 parseInt v = JSON.parseJSON v
 
-parseSolrPage :: JSON.Value -> JSON.Parser (V.Vector JSON.Object, Int)
-parseSolrPage = JSON.withObject "Solr response" $ \o -> do
-  r <- o JSON..: "response"
-  n <- parseInt =<< r JSON..: "numFound"
-  d <- r JSON..: "docs"
-  return (d, n)
+instance JSON.FromJSON SolrResponse where
+  parseJSON = JSON.withObject "Solr response" $ \o -> do
+    r <- o JSON..: "response"
+    s <- parseInt =<< r JSON..: "start"
+    n <- parseInt =<< r JSON..: "numFound"
+    d <- r JSON..: "docs" 
+    return SolrResponse
+      { solrStart = s
+      , solrNumFound = n
+      , solrDocs = d
+      }
 
-loadSolrPage :: HTTP.Request -> BSC.ByteString -> BSC.ByteString -> Int -> Int -> IO (V.Vector JSON.Object, Int)
-loadSolrPage req fq fl start rows =
-  parseM parseSolrPage . HTTP.responseBody =<< HTTP.httpJSON
+loadSolrOffset :: HTTP.Request -> BSC.ByteString -> BSC.ByteString -> Int -> Int -> IO SolrResponse
+loadSolrOffset req fq fl start rows =
+  HTTP.responseBody <$> HTTP.httpJSON
     (HTTP.setQueryString
       [ ("wt", Just "json")
       , ("hl", Just "off")
@@ -43,7 +51,12 @@ loadSolrPage req fq fl start rows =
       ] req)
 
 loadSolr :: HTTP.Request -> BSC.ByteString -> HSet.HashSet T.Text -> IO (V.Vector JSON.Object)
-loadSolr req fq fl = joinPages (loadSolrPage req fq fl') where
+loadSolr req fq fl = loop 0 100 where
+  loop _ 0 = return mempty
+  loop start rows = do
+    SolrResponse s n d <- loadSolrOffset req fq fl' start rows
+    let s' = s + length d
+    (d <>) <$> loop s' (n-s')
   fl' = BSC.intercalate "," $ map TE.encodeUtf8 $ HSet.toList fl
 
 -- Drupal acts like solr, some fields are just unused
