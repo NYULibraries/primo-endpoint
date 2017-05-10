4b8c0938d90ce9877c230e58cf52900f356ea0e9
Dylan Simon <dylan@dylex.net>
2017-05-10 07:09:57 -0400

    Treat unparsable ISAW fields as missing; add minimal config

--- Source/ISAW.hs
+++ Source/ISAW.hs
@@ -7,6 +7,7 @@ import           Control.Monad (void)
 import qualified Data.Aeson as JSON
 import qualified Data.Aeson.Types as JSON
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
 import qualified Data.Conduit as C
 import qualified Data.Conduit.Combinators as CC
 import           Data.Conduit.Attoparsec (sinkParser)
@@ -21,9 +22,10 @@ isawRequest :: HTTP.Request
 isawRequest = HTTP.parseRequest_ "http://isaw.nyu.edu/publications/awol-index/awol-index-json.zip"
 
 parseISAW :: JSON.Value -> JSON.Parser (Maybe Document)
-parseISAW (JSON.Object o) | JSON.Null <- HMap.lookupDefault JSON.Null "is_part_of" o =
-  return $ Just $ HMap.map parseValue o
-parseISAW _ = return Nothing
+parseISAW = JSON.withObject "isaw json" $ \o -> return $
+  case HMap.lookupDefault JSON.Null "is_part_of" o of
+    JSON.Null -> Just $ HMap.mapMaybe parseJSONM o
+    _ -> Nothing
 
 whileRight :: Monad m => C.Conduit (Either a b) m b
 whileRight = C.await >>= maybe
@@ -34,7 +36,9 @@ streamISAW :: C.Conduit (Either Z.ZipEntry BS.ByteString) IO Document
 streamISAW = mapM_ (\e -> entry e >> streamISAW) =<< C.await where
   entry (Left Z.ZipEntry{ Z.zipEntryName = n })
     | ".json" `BS.isSuffixOf` n =
-      mapM_ C.yield =<< parseM parseISAW =<< whileRight C..| sinkParser JSON.json'
+      mapM_ C.yield
+        =<< either (fail . ((BSC.unpack n ++ ": ") ++)) return . JSON.parseEither parseISAW
+        =<< whileRight C..| sinkParser JSON.json'
     | otherwise = return ()
   entry (Right _) = fail "Unexpected file data"
 
